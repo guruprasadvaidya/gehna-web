0af9aeaa21d54e35d7ac6580d551b8296716d77b
guruprasadvaidya <vaidyaguruprasad777@gmail.com>
2022-10-10 16:19:35 +0530

    change ho ja bhai

--- js/main.js
+++ js/main.js
@@ -352,13 +352,17 @@ hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive'
 window.onresize = () => {if (window.innerWidth > 760) mainnav.classList.remove('responsive')};
 
 
+// for smooth scrolling
 
-/*** Programming Notes **************************************
-  Arrow Functions - es6 syntactically compact alternative to a regular function expression
-  see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions/Arrow_functions
-  or https://www.w3schools.com/js/js_arrow_function.asp
-
-  classList property - https://developer.mozilla.org/en-US/docs/Web/API/Element/classList
-  */
-
-  
+$('a[href*="#"]').not('[href="#"]').not('[href="#0"]').click(function (t) {
+    if (location.pathname.replace(/^\//, "") == this.pathname.replace(/^\//, "") && location.hostname == this.hostname) {
+        var e = $(this.hash);
+        e = e.length ? e : $("[name=" + this.hash.slice(1) + "]"), e.length && (t.preventDefault(), $("html, body").animate({
+            scrollTop: e.offset().top
+        }, 600, function () {
+            var t = $(e);
+            if (t.focus(), t.is(":focus")) return !1;
+            t.attr("tabindex", "-1"), t.focus()
+        }))
+    }
+});
